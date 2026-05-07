c645343cccf2458bc6e42a5e0df8c9ecca3d7d43
Akash Kalamkar <akash.kalamkar@coditas.com>
2026-05-07 12:20:26 +0530

    first commit

---
+++ src/main/java/com/demo/genaidemo/GenaiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.demo.genaidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenaiDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GenaiDemoApplication.class, args);
+    }
+
+}
