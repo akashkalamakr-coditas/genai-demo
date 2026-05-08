31553693786ce603ce6efa078b6764039bf1c6cf
Akash Kalamkar <akash.kalamkar@coditas.com>
2026-05-08 08:12:35 +0530

    adding in main

--- src/main/java/com/demo/genaidemo/GenaiDemoApplication.java
+++ src/main/java/com/demo/genaidemo/GenaiDemoApplication.java
@@ -8,6 +8,7 @@ public class GenaiDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GenaiDemoApplication.class, args);
+        System.out.println("main method");
     }
 
 }
